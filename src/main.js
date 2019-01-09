5464c98563ba4bfabf1d98ac6dda2c669570472a
Roman <rbalik341@gmail.com>
2019-01-10 01:05:11 +0200

    const

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
-// console.log(PI); //error
-// const PI;
-//
-// const PI = 3.14159;
-// console.log(PI); //3.14159
-//
-// const PI;
-// console.log(PI); //error
-//
-//
+console.log(PI); //error
+const PI;
+
+const PI = 3.14159;
+console.log(PI); //3.14159
+
+const PI;
+console.log(PI); //error
 
 
 
-// const MATH = {
-//     PI : 3.14159
-// }
-// MATH.PI = 6;
-// console.log(MATH.PI);
+
+
+const MATH = {
+    PI : 3.14159
+}
+MATH.PI = 6;
+console.log(MATH.PI);
