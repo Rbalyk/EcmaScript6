067513a66bdfa635f9781e6d5ea91b76ea256925
Roman <rbalik341@gmail.com>
2019-01-09 23:49:46 +0200

    example

--- src/main.js
+++ src/main.js
@@ -8,3 +8,15 @@ if (true) {
     let name2 = 'Roman Balyk'
 }
 console.log(name2);
+
+
+var buttons = document.querySelectorAll("button");
+
+
+for (let i = 0; i < buttons.length; i++) {
+    var button = buttons[i];
+    button.innerText = i;
+    button.onclick = function (e) {
+        console.log(i)
+    }
+}
